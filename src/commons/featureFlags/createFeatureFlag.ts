25f04df62ed6152eef0812908e54edb1466c2137
tsammeow <97447335+tsammeow@users.noreply.github.com>
2025-02-01 16:21:35 +0800

    add getFlag to createFeatureFlag to allow sagas to get a flag

--- src/commons/featureFlags/createFeatureFlag.ts
+++ src/commons/featureFlags/createFeatureFlag.ts
@@ -1,15 +1,23 @@
-import { useDispatch } from "react-redux";
 import { FeatureFlagsActions } from ".";
 import { useFeatureFlag } from "./useFeatureFlag";
+import { store } from "src/pages/createStore";
 
+/**
+ * 
+ * @param featureFlag The name of the feature flag.
+ * @param defaultValue The default value of the feature flag.
+ * @returns [`featureFlag` the name of the feature flag, `useFlag` a React hook for this feature flag, `setFlag` to change the value of this feature flag, `resetFlag` to reset the value of this feature flag to its default value, `getFlag` to get the value of this feature flag]
+ */
 export function createFeatureFlag<T>(featureFlag: string, defaultValue: T):
     [string,
         () => T,
         (value: T) => ReturnType<typeof FeatureFlagsActions.setFlag>,
-        () => ReturnType<typeof FeatureFlagsActions.resetFlag>] {
-    const dispatch = useDispatch();
+        () => ReturnType<typeof FeatureFlagsActions.resetFlag>,
+        () => T] {
+    const dispatch = store.dispatch;
     const useFlag = () => useFeatureFlag<T>(featureFlag, defaultValue);
     const setFlag = (value: T) => dispatch(FeatureFlagsActions.setFlag({ featureFlag, value }));
     const resetFlag = () => dispatch(FeatureFlagsActions.resetFlag({ featureFlag }));
-    return [featureFlag, useFlag, setFlag, resetFlag];
+    const getFlag = () => store.getState().featureFlags.modifiedFlags[featureFlag] ?? defaultValue;
+    return [featureFlag, useFlag, setFlag, resetFlag, getFlag];
 }
