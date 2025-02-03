829cfccc645ba9e405684419db8bd5b91533f3d5
tsammeow <97447335+tsammeow@users.noreply.github.com>
2025-02-04 07:52:33 +0800

    turn conductor feature off by default

--- src/features/conductor/featureConductor.ts
+++ src/features/conductor/featureConductor.ts
@@ -1,3 +1,3 @@
 import { FeatureFlag } from "src/commons/featureFlags/FeatureFlag";
 
-export const featureConductor: FeatureFlag<boolean> = ["conductor.enable", true] as const;
+export const featureConductor: FeatureFlag<boolean> = ["conductor.enable", false] as const;
